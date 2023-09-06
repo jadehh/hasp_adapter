1aa9db28c241a03a5627873e962241fc273679fe
jade <jadehh@live.com>
2023-09-06 10:21:38 +0800

    * 更新测试hasp demo

--- main.cpp
+++ main.cpp
@@ -17,17 +17,8 @@ void wait4key(char *hint)
         ;
 }
 int main() {
-    HaspStruct haspStruct;
-    HaspStruct haspStruct1 = login(0);
-    printf("login status = %d\n",haspStruct.status);
-    haspStruct = getInfo();
+    HaspStruct haspStruct = getInfo();
     printf("status:%d,获取info状态:%s\n", haspStruct.status,haspStruct.info);
-    haspStruct = getSessionInfo(301);
-    printf("status:%d,获取Session info状态:%s\n",haspStruct.status, haspStruct.info);
-    logout(haspStruct1.handle);
-    HaspStruct haspStruct2 = login(301);
-//    wait4key("contine");
-    logout(haspStruct2.handle);
-    printf("logout:%d",haspStruct2.handle);
+
     return 0;
 };
